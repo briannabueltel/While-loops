a4585d9158d8756168c2bdc93f084da276131a2d
Brianna Bueltel <brianna.bueltel@mrjaffesclass.com>
2022-09-27 13:55:17 -0700

    finished while loops

---
+++ HelloWorld.java
@@ -0,0 +1,34 @@
+public class HelloWorld
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+
+    public void run() {
+        int i = 0;
+        while (i < 15) {
+            System.out.println(i);
+            i++;
+        } 
+        int k = 10;
+        while (k > 5) {
+            System.out.println(k);
+            k--;
+        }
+        int a = 0;
+        int b = 25;
+        while (a < 10 && b <= 20) {
+            System.out.println(a + " " + b);
+            a++;
+            b++;
+        }
+    }
+
+    public static void main(String[] args)
+    {        
+        HelloWorld hw = new HelloWorld();
+        hw.run();
+    }
+}
